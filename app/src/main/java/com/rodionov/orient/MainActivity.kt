4ee70f462ce8279c7375e78fe7505ad463e7e1e0
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2019-09-06 00:03:42 +0400

    first commit

---
+++ app/src/main/java/com/rodionov/orient/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rodionov.orient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
