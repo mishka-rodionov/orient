821da252feccd23b916b68c43d77cf959793c9f9
rodionov <rodionov.mikhail.a@gmail.com>
2019-09-09 16:44:46 +0400

    add bottom navigation view

---
+++ app/src/main/java/com/rodionov/orient/app/BottomNavigationBarActivity.kt
@@ -0,0 +1,13 @@
+package com.rodionov.orient.app
+
+import android.os.Bundle
+
+/**
+ * Created by rodionov on 08.09.2019.
+ */
+class BottomNavigationBarActivity: MainActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/rodionov/orient/app/MainActivity.kt
+++ app/src/main/java/com/rodionov/orient/app/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.rodionov.orient
+package com.rodionov.orient.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.rodionov.orient.R
 
 open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_bottom_navigation_bar)
     }
 }
